e41d3d5d2119babcd27f55cbadd5fae091990083
Mohamed Ali Hafi <hfdali123@gmail.com>
2020-07-21 17:15:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import desk from "./desk.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    <div  >  
+ 
+ <h1 style={{'textAlign' : "center"}}    >Your name here</h1>  
+
+     
+<div className="doudou">
+
+       <img src="nature.jpg"  alt="nature" className="img123"/>
+
+
+          <img src={desk} alt="desk" className="img123"/>  
+
+</div>
+          </div>  
+
+<video style={{ width: 320 , height:240 , marginLeft:'35%' , padding: 50}} controls>  
+
+   <source src="calopsitte-qui-chante.mp4" type="video/mp4" /> 
+
+</video> 
+
+    </div> 
+    
   );
 }
 
